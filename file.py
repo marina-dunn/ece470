9f22a2f2b15e21a75e37d3cf9c865f200c5c21b2
Marina <marjdunn@shaw.ca>
2019-07-10 20:04:35 -0700

    Got parserworking

--- file.py
+++ file.py
@@ -1,8 +1,35 @@
-# import numpy
 import csv
+import random
 
-with open('echocardiogram.csv')as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        print(', '.join(row))
+def csvParser(filename):
+    with open(filename)as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        line_count = 0
+        vals = []
+        for row in csv_reader:
+            temp = []
+            if row[0] != 'survival':
+                for val in row:
+                    if val == '?': # Temporarily setting ? values to zero
+                        val = 0
+                    c = float(val)
+                    temp.append(c)
+                vals.append(temp)
+        return vals
+
+def crossover(parent1, parent2):
+    # What I want to do here is take in the two parents
+    # use a random value 
+    len1 = len(parent1)
+    len2 = len(parent2)
+    len1 = random.randrange(range(len1))
+    print(len1)
+
+def main():
+    res = csvParser('trainingdata.csv')
+    # here I am going to run it through the AI and ML 
+    test = csvParser('testingdata.csv')
+    # here I am going to run it through the AI and test the ML
+    
+if __name__ == "__main__":
+    main()
