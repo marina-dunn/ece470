ae0aa7376ec567f97b279871530d6283860ed839
Marina <marjdunn@shaw.ca>
2019-07-10 20:30:48 -0700

    Made progress on crossover and mutation fns

--- file.py
+++ file.py
@@ -18,15 +18,38 @@ def csvParser(filename):
         return vals
 
 def crossover(parent1, parent2):
-    # What I want to do here is take in the two parents
-    # use a random value 
+    # TODO: make this more efficient (less variables)
+    # This function takes in two parents and exchanges random values within them
     len1 = len(parent1)
     len2 = len(parent2)
-    len1 = random.randrange(range(len1))
-    print(len1)
+    rand1 = random.randrange(len1)
+    rand2 = random.randrange(len2)
+    cross1 = parent1
+    cross2 = parent2
+    temp = cross1[rand1]
+    cross1[rand1] = cross2[rand2]
+    cross2[rand2] = temp
+    return cross1, cross2
+
+def mutation(individual):
+    # In here we are taking in an individual and applying the flip bit mutation
+    rand = random.randrange(len(individual))
+    val = individual[rand]
+    if val == 0:
+        individual[rand] = 1
+    else:
+        individual[rand] = 0
+    return individual 
 
 def main():
     res = csvParser('trainingdata.csv')
+    # This next part is the crossover fn
+    for val in range(len(res)):
+        if val != 0:
+            crossed1, crossed2 = crossover(res[val-1], res[val])
+            res[val-1] = crossed1
+            res[val] = crossed2
+    # This is where the mutation would go when we have it in the correct binary format
     # here I am going to run it through the AI and ML 
     test = csvParser('testingdata.csv')
     # here I am going to run it through the AI and test the ML
